1a30a51e2d1b78aba07af55a82c0da74daeda875
Seong57 <Seong57@naver.com>
2023-09-12 23:29:32 +0900

    security config 설정 완료

---
+++ api/src/main/java/com/example/sideporoject/config/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.sideporoject.config.security;
+
+import com.example.sideporoject.filter.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest -> {
+                    authorizeRequest.requestMatchers("/api/admin/**")
+                            .hasRole("ADMIN");
+                    authorizeRequest.requestMatchers("/api/**")
+                            .hasRole("USER");
+                    authorizeRequest.anyRequest().permitAll();
+                })
+                .sessionManagement(config -> {
+                    config.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .formLogin(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+}
