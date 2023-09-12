057633f63e42e7a199522d7a4e81c01ae85790fe
Seong57 <Seong57@naver.com>
2023-09-12 21:37:25 +0900

    start project

---
+++ api/src/main/java/com/example/sideporoject/ApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.sideporoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
