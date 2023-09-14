abd55c3109a5770300aabc72a217ef7d020d8944
Seong57 <Seong57@naver.com>
2023-09-14 20:00:30 +0900

    tokenResponse 추가

---
+++ api/src/main/java/com/example/sideporoject/domain/token/model/TokenResponse.java
@@ -0,0 +1,22 @@
+package com.example.sideporoject.domain.token.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponse {
+
+    private String accessToken;
+    private LocalDateTime accessTokenExpiredAt;
+
+    private String refreshToken;
+
+    private LocalDateTime refreshTokenExpiredAt;
+}
