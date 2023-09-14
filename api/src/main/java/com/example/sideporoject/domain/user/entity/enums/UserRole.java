57e117f36d3e187831209e5e2dd9762693066363
Seong57 <Seong57@naver.com>
2023-09-14 19:14:14 +0900

    user role 추가

---
+++ api/src/main/java/com/example/sideporoject/domain/user/entity/enums/UserRole.java
@@ -0,0 +1,15 @@
+package com.example.sideporoject.domain.user.entity.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum UserRole {
+
+    MASTER("마스터"),
+    MANAGER("매니저"),
+    USER("일반 유저"),
+
+    ;
+
+    private String description;
+}
