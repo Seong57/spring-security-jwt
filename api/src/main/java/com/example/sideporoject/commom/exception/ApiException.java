59ba96845b40bf4d96dc6f55ecb89d5e614e3ac4
Seong57 <Seong57@naver.com>
2023-09-15 10:57:35 +0900

    errorDescription 추가

--- api/src/main/java/com/example/sideporoject/commom/exception/ApiException.java
+++ api/src/main/java/com/example/sideporoject/commom/exception/ApiException.java
@@ -8,23 +8,40 @@ import lombok.Getter;
 public class ApiException extends RuntimeException{
 
     private final ErrorCodeIfs errorCodeIfs;
+    private final String errorDescription;
 
-    public ApiException(ErrorCodeIfs errorCodeIfs) {
+    public ApiException(ErrorCodeIfs errorCodeIfs){
+        super(errorCodeIfs.getErrorDescription());
         this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorCodeIfs.getErrorDescription();
     }
 
-    public ApiException( ErrorCodeIfs errorCodeIfs,String message) {
+    public ApiException(ErrorCodeIfs errorCodeIfs, String errorDescription) {
+        this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorDescription;
+    }
+
+    public ApiException(String message, ErrorCodeIfs errorCodeIfs, String errorDescription) {
         super(message);
         this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorDescription;
     }
 
-    public ApiException(String message, Throwable cause, ErrorCodeIfs errorCodeIfs) {
+    public ApiException(String message, Throwable cause, ErrorCodeIfs errorCodeIfs, String errorDescription) {
         super(message, cause);
         this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorDescription;
     }
 
-    public ApiException(Throwable cause, ErrorCodeIfs errorCodeIfs) {
+    public ApiException(Throwable cause, ErrorCodeIfs errorCodeIfs, String errorDescription) {
         super(cause);
         this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorDescription;
+    }
+
+    public ApiException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ErrorCodeIfs errorCodeIfs, String errorDescription) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.errorCodeIfs = errorCodeIfs;
+        this.errorDescription = errorDescription;
     }
 }
---
+++ api/src/main/java/com/example/sideporoject/domain/token/TokenProvider.java
@@ -0,0 +1,10 @@
+package com.example.sideporoject.domain.token;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class TokenProvider {
+
+}
