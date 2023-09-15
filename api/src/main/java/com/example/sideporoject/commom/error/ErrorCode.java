4b613e204c1c3766a2fca2cead5b5a1547245eb4
Seong57 <Seong57@naver.com>
2023-09-15 10:57:49 +0900

    FORBIDDEN_ERROR 추가

--- api/src/main/java/com/example/sideporoject/commom/error/ErrorCode.java
+++ api/src/main/java/com/example/sideporoject/commom/error/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode implements ErrorCodeIfs{
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청"),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "서버 내 오류"),
     NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "null point"),
+    FORBIDDEN_ERROR(HttpStatus.FORBIDDEN.value(), 403, "권한 없음"),
     ;
     private final Integer httpStatusCode;   // 클라이언트에 보여줄 status code
     private final Integer errorCode;    // 우리 서버 내부에서 사용할 에러 코드
