1958ef4eab5f1b44070ced4cff3c92802a3829f4
Seong57 <Seong57@naver.com>
2023-09-15 10:57:02 +0900

    tokenConverter 추가

---
+++ api/src/main/java/com/example/sideporoject/domain/token/converter/TokenConverter.java
@@ -0,0 +1,28 @@
+package com.example.sideporoject.domain.token.converter;
+
+import com.example.sideporoject.commom.error.ErrorCode;
+import com.example.sideporoject.commom.exception.ApiException;
+import com.example.sideporoject.domain.token.dto.TokenDto;
+import com.example.sideporoject.domain.token.model.TokenResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class TokenConverter {
+
+    public TokenResponse toResponse(
+            TokenDto accessToken,
+            TokenDto refreshToken
+    ){
+        Objects.requireNonNull(accessToken, ()->{throw new ApiException(ErrorCode.NULL_POINT);});
+        Objects.requireNonNull(refreshToken, ()->{throw new ApiException(ErrorCode.NULL_POINT);});
+
+        return TokenResponse.builder()
+                .accessToken(accessToken.getToken())
+                .accessTokenExpiredAt(accessToken.getExpiredAt())
+                .refreshToken(refreshToken.getToken())
+                .refreshTokenExpiredAt(refreshToken.getExpiredAt())
+                .build();
+    }
+}
