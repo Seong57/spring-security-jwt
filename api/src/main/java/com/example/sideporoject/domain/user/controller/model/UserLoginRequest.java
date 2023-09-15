b43a78ce64a4256f6bae570895f51d792ef153e2
Seong57 <Seong57@naver.com>
2023-09-15 13:57:41 +0900

    validation 추가

--- api/src/main/java/com/example/sideporoject/domain/user/controller/model/UserLoginRequest.java
+++ api/src/main/java/com/example/sideporoject/domain/user/controller/model/UserLoginRequest.java
@@ -1,5 +1,7 @@
 package com.example.sideporoject.domain.user.controller.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserLoginRequest {
 
+    @Email(message = "이메일 형식으로 입력하세요")
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
