86e74d0b22e96d31f0d14ec694a67322d275b483
Seong57 <Seong57@naver.com>
2023-09-15 19:05:58 +0900

    update

--- api/src/main/java/com/example/sideporoject/domain/auth/UserDetailsImpl.java
+++ api/src/main/java/com/example/sideporoject/domain/auth/UserDetailsImpl.java
@@ -2,10 +2,7 @@ package com.example.sideporoject.domain.auth;
 
 import com.example.sideporoject.domain.user.entity.enums.UserRole;
 import com.example.sideporoject.domain.user.entity.enums.UserStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
