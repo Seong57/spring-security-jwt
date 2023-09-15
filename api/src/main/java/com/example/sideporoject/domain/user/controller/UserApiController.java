a0782eb4bad27e3a169c9db1b45df30fcb1a8f03
Seong57 <Seong57@naver.com>
2023-09-15 19:05:24 +0900

    test용 매핑 추가

--- api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
+++ api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
@@ -1,7 +1,11 @@
 package com.example.sideporoject.domain.user.controller;
 
 import com.example.sideporoject.commom.costomresponse.reponse.Response;
+import com.example.sideporoject.domain.auth.UserDetailsImpl;
+import com.example.sideporoject.domain.health.HealthResponse;
+import com.example.sideporoject.domain.user.HealthCall;
 import com.example.sideporoject.domain.user.controller.model.UserResponse;
+import com.example.sideporoject.domain.user.entity.UserEntity;
 import com.example.sideporoject.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,8 +14,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,14 +28,30 @@ public class UserApiController {
     private final UserService userService;
 
     @GetMapping("/me")
-    public Response<UserDetails> me(
+    public Response<UserResponse> me(
             @Parameter(hidden = true)
             @AuthenticationPrincipal
-            UserDetails userDetails
+            UserDetailsImpl userDetails
     ){
 
+        UserResponse response = UserResponse.builder()
+                .id(userDetails.getId())
+                .name(userDetails.getUsername())
+                .email(userDetails.getEmail())
+                .build();
 
-        return Response.OK(userDetails);
+        return Response.OK(response);
 
+    }
+    @GetMapping("/test")
+    public Response<HealthResponse> test(){
+
+        HealthResponse health = HealthResponse.builder()
+                .id(1L)
+                .name("health")
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        return Response.OK(health);
     }
 }
