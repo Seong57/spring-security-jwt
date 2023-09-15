d586da8cc5b28ad3c1e801a13f1dfc50e252ddaf
Seong57 <Seong57@naver.com>
2023-09-15 13:56:11 +0900

    수정

--- api/src/main/java/com/example/sideporoject/filter/JwtFilter.java
+++ api/src/main/java/com/example/sideporoject/filter/JwtFilter.java
@@ -1,5 +1,8 @@
 package com.example.sideporoject.filter;
 
+import com.example.sideporoject.commom.error.TokenErrorCode;
+import com.example.sideporoject.commom.exception.ApiException;
+import com.example.sideporoject.domain.token.JwtUtils;
 import com.example.sideporoject.domain.token.service.TokenService;
 import com.example.sideporoject.domain.token.tokenhelper.TokenHelper;
 import com.example.sideporoject.domain.user.entity.UserEntity;
@@ -9,10 +12,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -25,27 +33,51 @@ import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter  {
 
-    private final TokenService tokenService;
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
 
+
+    /* @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String authorizationToken = req.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization token : {}", authorizationToken);
+
+        if (authorizationToken == null) {
+            log.info("authorization token이 없습니다.");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String userEmail = tokenService.validationToken(authorizationToken);
+        log.info("userEmail : {}", userEmail);
+        chain.doFilter(request, response);
+    }*/
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        /*HttpServletRequest req = (HttpServletRequest) request;
-        final String authorization = req.getHeader(HttpHeaders.AUTHORIZATION);
-        log.info("authorization : {}", authorization);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization token : {}", authorizationToken);
 
-        Long userId = tokenService.validationToken(authorization);
+        if (authorizationToken == null) {
+            log.info("authorization token이 없습니다.");
+            throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
+        }
+
+        String userEmail = jwtUtils.validationToken(authorizationToken);
+        log.info("userEmail : {}", userEmail);
 
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(
-                        userId, null, List.of(new SimpleGrantedAuthority("USER"))
-                );
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
 
-        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);*/
-        chain.doFilter(request, response);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        System.out.println("authentication = " + authentication.getPrincipal());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
     }
 }
