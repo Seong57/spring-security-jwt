9d4ceab2f0dbb7b8d2e35d01f975ffcfb609302a
Seong57 <Seong57@naver.com>
2023-09-15 13:57:07 +0900

    변경

--- api/src/main/java/com/example/sideporoject/domain/token/service/TokenService.java
+++ api/src/main/java/com/example/sideporoject/domain/token/service/TokenService.java
@@ -2,9 +2,14 @@ package com.example.sideporoject.domain.token.service;
 
 import com.example.sideporoject.commom.error.ErrorCode;
 import com.example.sideporoject.commom.exception.ApiException;
+import com.example.sideporoject.domain.auth.UserDetailsServiceImpl;
 import com.example.sideporoject.domain.token.dto.TokenDto;
 import com.example.sideporoject.domain.token.tokenhelper.TokenHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -17,25 +22,26 @@ public class TokenService {
 
     private final TokenHelper tokenHelper;
 
-    public TokenDto issueAccessToken(Long userId) {
+    public TokenDto issueAccessToken(String userEmail) {
         HashMap<String, Object> data = new HashMap<>();
-        data.put("userId", userId);
+        data.put("userEmail", userEmail);
         return tokenHelper.issueAccessToken(data);
     }
 
-    public TokenDto issueRefreshToken(Long userId) {
+    public TokenDto issueRefreshToken(String userEmail) {
         HashMap<String, Object> data = new HashMap<>();
-        data.put("userId", userId);
+        data.put("userEmail", userEmail);
         return tokenHelper.issueRefreshToken(data);
     }
 
-    public Long validationToken(String token){
+    public String validationToken(String token){
 
         Map<String, Object> data = tokenHelper.validationTokenWithThrow(token);
-        Object userId = data.get("userId");
-        Objects.requireNonNull(userId, () -> {
+        Object userEmail = data.get("userEmail");
+        Objects.requireNonNull(userEmail, () -> {
             throw new ApiException(ErrorCode.NULL_POINT);
         });
-        return Long.parseLong(userId.toString());
+        return userEmail.toString();
     }
+
 }
