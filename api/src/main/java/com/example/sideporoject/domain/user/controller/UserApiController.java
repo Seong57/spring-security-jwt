e0c5f1af693f60d3da149e679f1f170b532a8fb6
Seong57 <Seong57@naver.com>
2023-09-15 13:57:20 +0900

    me controller 추가

--- api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
+++ api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
@@ -3,8 +3,12 @@ package com.example.sideporoject.domain.user.controller;
 import com.example.sideporoject.commom.costomresponse.reponse.Response;
 import com.example.sideporoject.domain.user.controller.model.UserResponse;
 import com.example.sideporoject.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +20,15 @@ public class UserApiController {
 
     private final UserService userService;
 
-    /*@GetMapping("/me")
-    public Response<UserResponse> me(
-            Authentication authentication
+    @GetMapping("/me")
+    public Response<UserDetails> me(
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal
+            UserDetails userDetails
     ){
 
-    }*/
+
+        return Response.OK(userDetails);
+
+    }
 }
