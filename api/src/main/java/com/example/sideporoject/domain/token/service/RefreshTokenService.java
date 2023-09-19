c141beef962e0c67dfcc997dd002cdffe9cee3f6
Seong57 <Seong57@naver.com>
2023-09-20 00:27:35 +0900

    RefreshTokenService

---
+++ api/src/main/java/com/example/sideporoject/domain/token/service/RefreshTokenService.java
@@ -0,0 +1,58 @@
+package com.example.sideporoject.domain.token.service;
+
+
+import com.example.sideporoject.commom.error.TokenErrorCode;
+import com.example.sideporoject.commom.error.UserErrorCode;
+import com.example.sideporoject.commom.exception.ApiException;
+import com.example.sideporoject.domain.token.dto.RefreshTokenDto;
+import com.example.sideporoject.domain.token.entity.RefreshToken;
+import com.example.sideporoject.domain.token.repository.RefreshTokenRepository;
+import com.example.sideporoject.domain.user.controller.model.UserResponse;
+import com.example.sideporoject.domain.user.entity.UserEntity;
+import com.example.sideporoject.domain.user.repository.UserRepository;
+import com.example.sideporoject.security.token.dto.TokenDto;
+import com.example.sideporoject.security.token.model.TokenResponse;
+import com.example.sideporoject.security.token.service.TokenService;
+import com.example.sideporoject.security.token.tokenhelper.TokenHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+
+    public void deleteRefreshToken(RefreshTokenDto refreshTokenDto) {
+
+        refreshTokenRepository.findByValue(refreshTokenDto.getRefreshToken())
+                .ifPresent(refreshTokenRepository::delete);
+    }
+
+    public TokenResponse reissueAccessToken(RefreshTokenDto refreshTokenDto) {
+        RefreshToken refreshToken = refreshTokenRepository.findByValue(refreshTokenDto.getRefreshToken())
+                .orElseThrow(() -> new ApiException(TokenErrorCode.INVALID_TOKEN));
+
+        String userEmail = tokenService.validationTokenWithUserEmail(refreshToken.getValue());
+
+        UserEntity userEntity = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+
+        Long userId = userEntity.getId();
+
+        TokenDto accessToken = tokenService.issueAccessToken(userId);
+
+        return TokenResponse.builder()
+                .accessToken(accessToken.getToken())
+                .accessTokenExpiredAt(accessToken.getExpiredAt())
+                .refreshToken(refreshToken.getValue())
+                .refreshTokenExpiredAt(refreshToken.getExpiredAt())
+                .build();
+
+    }
+}
