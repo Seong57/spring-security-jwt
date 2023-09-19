2ad124bc247b993ae5bf01e53d11a9ff391bc903
Seong57 <Seong57@naver.com>
2023-09-20 00:24:46 +0900

    logout, refreshToken컨트롤러 추가

--- api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
+++ api/src/main/java/com/example/sideporoject/domain/user/controller/UserApiController.java
@@ -3,20 +3,18 @@ package com.example.sideporoject.domain.user.controller;
 import com.example.sideporoject.commom.costomresponse.reponse.Response;
 import com.example.sideporoject.domain.auth.UserDetailsImpl;
 import com.example.sideporoject.domain.health.HealthResponse;
-import com.example.sideporoject.domain.user.HealthCall;
+import com.example.sideporoject.domain.token.dto.RefreshTokenDto;
+import com.example.sideporoject.domain.token.service.RefreshTokenService;
 import com.example.sideporoject.domain.user.controller.model.UserResponse;
-import com.example.sideporoject.domain.user.entity.UserEntity;
 import com.example.sideporoject.domain.user.service.UserService;
-import io.swagger.v3.oas.annotations.Hidden;
+import com.example.sideporoject.security.token.model.TokenResponse;
+import com.example.sideporoject.security.token.service.TokenService;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +24,8 @@ import java.time.LocalDateTime;
 public class UserApiController {
 
     private final UserService userService;
+    private final RefreshTokenService refreshTokenService;
+    private final TokenService tokenService;
 
     @GetMapping("/me")
     public Response<UserResponse> me(
@@ -33,16 +33,14 @@ public class UserApiController {
             @AuthenticationPrincipal
             UserDetailsImpl userDetails
     ){
-
         UserResponse response = UserResponse.builder()
-                .id(userDetails.getId())
-                .name(userDetails.getUsername())
+                .name(userDetails.getName())
                 .email(userDetails.getEmail())
                 .build();
 
         return Response.OK(response);
-
     }
+
     @GetMapping("/test")
     public Response<HealthResponse> test(){
 
@@ -53,5 +51,25 @@ public class UserApiController {
                 .build();
 
         return Response.OK(health);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<RefreshTokenDto> logout(
+            @RequestBody
+            @Valid
+            RefreshTokenDto refreshTokenDto
+    ){
+        refreshTokenService.deleteRefreshToken(refreshTokenDto);
+
+        return ResponseEntity.status(200).body(refreshTokenDto);
+    }
+
+    @PostMapping("/refresh-token")
+    public Response<TokenResponse> refreshToken(
+            @RequestBody
+            RefreshTokenDto refreshTokenDto
+    ){
+        TokenResponse tokenResponse = refreshTokenService.reissueAccessToken(refreshTokenDto);
+        return Response.OK(tokenResponse);
     }
 }
