6c036b8d11a7a2fe93a42c06ff1508b6320721d7
Seong57 <Seong57@naver.com>
2023-09-20 19:35:46 +0900

    webconfig 삭제

---
+++ api/src/main/java/com/example/sideporoject/config/web/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.sideporoject.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
+/*@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebSecurity webSecurity(WebSecurity webSecurity) {
+        webSecurity.ignoring().requestMatchers("/open-api/**");
+        return webSecurity;
+    }
+
+}*/
